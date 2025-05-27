79902b827797d2407241e08555a7858c1e675044
Sanikaugale28 <ugalesanika28@gmail.com>
2025-05-27 13:39:47 +0000

    Math operators

---
+++ math.js
@@ -0,0 +1,51 @@
+let num= Math.PI;
+console.log(num);
+
+let num1= Math.SQRT2;
+console.log(num1);
+
+let num2= Math.SQRT1_3;
+console.log(num2);
+
+let num3= 24;
+let result = Math.sqrt(num3);
+console.log(result);
+
+let num4= Math.round(4.4);
+console.log(num4);
+
+let num5= Math.ceil(4.6);
+console.log(num5);
+
+let num6= Math.sign(0);
+console.log(num6);
+
+let num7= Math.pow(7,4);
+console.log(num7);
+
+
+let num8= Math.sin(90* Math.PI/180);
+console.log(num8);
+
+let num9= Math.min(0,34,89,-98,7);
+console.log(num9);
+
+let num01= Math.max(87,67,9,-98,76);
+console.log(num01);
+
+let num02= Math.random();
+console.log(num02);
+
+let num03=Math.log10(1000);
+console.log(num03);
+
+
+
+
+
+
+
+
+
+
+
